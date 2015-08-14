9fa2fe14106b6e481f1674e89cf3432a60f53426
Matt Mo <matthew.l.mo@gmail.com>
2015-08-14 01:12:45 -0700

    [Settings] - add initial files

---
+++ Tip Calculator/Tip Calculator/SettingsViewController.m
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.m
+//  Tip Calculator
+//
+//  Created by admin on 8/13/15.
+//  Copyright (c) 2015 mattmo. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
