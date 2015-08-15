7af8db3a145c27f841ad9bb2bb7db61ef49ccb0d
Matt Mo <matthew.l.mo@gmail.com>
2015-08-14 17:13:49 -0700

    [Cleaning] - remove helper no longer needed

--- Tip Calculator/Tip Calculator/ViewController.m
+++ Tip Calculator/Tip Calculator/ViewController.m
@@ -83,7 +83,6 @@
         tipCell.tipPercentage = [NSNumber numberWithInteger:indexPath.row];
         [tipCell updateCell:[self.billTotalField.text doubleValue]];
     }
-    cell.backgroundColor = [self randomColor];
     return cell;
 }
 
@@ -103,13 +102,6 @@
             [tipCell updateCell:[newBillValue doubleValue]];
         }
     }
-}
-
-- (UIColor *)randomColor {
-    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
-    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
-    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
-    return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
 }
 
 @end
